7aebac67a9c994751703535ea10635e561b1cacb
michelle0716 <1031983332b>
2018-11-21 15:02:28 +0800

    Initial commit

---
+++ app/src/main/java/com/antonioleiva/mvpexample/app/login/LoginInteractor.java
@@ -0,0 +1,33 @@
+package com.antonioleiva.mvpexample.app.login;
+
+import android.os.Handler;
+import android.text.TextUtils;
+
+/**
+ * 用户名和密码数据存储，作为model
+ */
+public class LoginInteractor {
+
+    interface OnLoginFinishedListener {
+        void onUsernameError();
+
+        void onPasswordError();
+
+        void onSuccess();
+    }
+
+    public void login(final String username, final String password, final OnLoginFinishedListener listener) {
+        // Mock login. I'm creating a handler to delay the answer a couple of seconds
+        new Handler().postDelayed(() -> {
+            if (TextUtils.isEmpty(username)) {
+                listener.onUsernameError();
+                return;
+            }
+            if (TextUtils.isEmpty(password)) {
+                listener.onPasswordError();
+                return;
+            }
+            listener.onSuccess();
+        }, 2000);
+    }
+}
